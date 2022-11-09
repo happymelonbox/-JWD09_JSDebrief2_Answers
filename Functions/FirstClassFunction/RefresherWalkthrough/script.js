18446b9df5fc247f0468f60ac3d4a5c320833d61
Justin Berger Howes <howes.j.j@gmail.com>
2022-11-10 00:40:54 +1100

    first class function walkthrough answer added

---
+++ Functions/FirstClassFunction/RefresherWalkthrough/script.js
@@ -0,0 +1,33 @@
+function runFiveMiles(){
+    console.log('Go for a five-mile run')
+}
+
+function liftWeights(){
+    console.log('Pump iron')
+}
+
+function swimFortyLaps(){
+    console.log('Swim 40 laps')
+}
+
+function excerciseRoutine(postRunActivity){
+    runFiveMiles()
+    postRunActivity()
+}
+
+function morningRoutine(exercise){
+    let breakfast = null
+    if (exercise === liftWeights){
+        breakfast = 'protein bar'
+    } else if (exercise === swimFortyLaps){
+        breakfast = 'kale smoothie'
+    } else {
+        breakfast = 'granola'
+    }
+
+    exerciseRoutine(exercise)
+
+    return function(){
+        console.log(`Nom, nom, nom, this ${breakfast} is delicious`)
+    }
+}
