fc18912b3ee14035405b82402f75b4933855774f
Justin Berger Howes <howes.j.j@gmail.com>
2022-11-10 01:03:49 +1100

    scope activity answers added

---
+++ Scope/script.js
@@ -0,0 +1,19 @@
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Mary'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Tim'
+}
